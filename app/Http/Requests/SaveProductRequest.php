1004413a4c6370846ad42ab26f58153f9808b899
Cornelius <cornelius.udeh@yahoo.com>
2023-02-25 03:07:46 +0100

    implement backend logic

---
+++ app/Http/Requests/SaveProductRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SaveProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'productName' => 'string|required',
+            'productQuantity' => 'decimal:0,3|required',
+            'productPrice' => 'decimal:0,3|required',
+        ];
+    }
+}
